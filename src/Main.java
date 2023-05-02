e25382b6d1e04b8c6a3ce61f5400f5195f5064ec
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-05-02 23:28:42 +0430

    in class Main sorted people with lastName length  is written

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
         List<Person> people = getPerson();
 //        people.stream().sorted(Comparator.comparing(Person::getAge)).
 //                collect(Collectors.toList()).forEach(System.out::println);
-        people.stream().sorted(Comparator.comparing(Person -> Person.getAddress().getPostalCode()))
-                .collect(Collectors.toList()).forEach(System.out::println);
+//        people.stream().sorted(Comparator.comparing(Person -> Person.getAddress().getPostalCode()))
+//                .collect(Collectors.toList()).forEach(System.out::println);
+//        people.stream().sorted(Comparator.comparing(Person->Person.getLastName().length())).
+//                collect(Collectors.toList()).forEach(System.out::println);
+
 
 
 
@@ -19,7 +22,7 @@ public class Main {
                         new Address("semnan", 1, "azadi", "3594734473")),
                 new Person("reza", "sadghiFard", 22,
                         new Address("tehran", 5, "moallem", "3514747532")),
-                new Person("saman", "samani", 18,
+                new Person("saman", "samaniaaaa", 18,
                         new Address("shiraz", 2, "kamali", "3598765432")),
                 new Person("amirAli", "rezaee", 15,
                         new Address("tehran", 22, "navab", "3514747532"))
